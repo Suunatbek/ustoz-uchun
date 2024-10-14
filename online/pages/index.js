822770f306865489aaedd5ec4abd5ec060ccbf84
arabiy <arabiy@gmail.com>
2024-10-14 19:01:10 +0500

    3 martani 1-chisi

--- online/pages/index.js
+++ online/pages/index.js
@@ -1,115 +1,110 @@
 import Image from "next/image";
-import localFont from "next/font/local";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
-export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+function vdc() {
+    return(
+        <div>
+          
+          <div class="flex justify-between">
+            <div class="flex" >
+                <div class="ml-20">
+                <div class="text-6xl font-extrabold">zeio</div>
+                <div class="font-light text-2xl">Design</div>
+                </div>
+               <div class="flex mt-9 ml-8 gap-11 font-medium text-1xl">
+               <div class="">Landing</div>
+                <div class="">Company</div>
+                <div class="">CMS Pages</div>
+                <div class="">Utility</div>
+               </div>
+            </div>
+            <div class="flex">
+                <div class="mt-5 h-16 text-white flex justify-center text-center w-36 bg-black pt-5">$129 Buy Now</div>
+                
+                <div class="mt-8"> <img class=" h-8" src="qid.png" alt="" /></div>
+
+               <div> <img class="h-14 mt-5 text-amber-400" src="https://media.istockphoto.com/id/1359210128/vector/shopping-cart-with-purchases-and-foods-icon-line-vector-illustration.jpg?s=612x612&w=0&k=20&c=N2hlzwOPMHjSaaBsy2a_EgqIXizrpH45yueojCzwb-w=" alt="" /></div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div class="flex justify-center gap-20">
+            <div>
+                <div class="text-9xl font-light mt-52">A stylish UI Kit</div>
+                <div class="mt-8 text-4xl font-medium">for all web creatives</div>
+                <div class="mt-10 text-neutral-500 text-3xl">Zero is a Webflow Ecommerce and CMS UI kit including  <br /> several carefully designed page layouts.</div>
+                <div class="flex mt-32 gap-14">
+                <div>smooth <br />interactions</div>
+                <div>
+                many
+                <br />
+                page layouts
+                </div>
+                <div>
+                fully
+                <br />
+                responsive
+                </div>
+                <div>
+                fast
+                <br />
+                customization
+                </div>
+                </div>
+            </div>
+            <div>
+            <img src="https://s3-alpha-sig.figma.com/img/34ca/5858/104d3f3828d19ac0097f352055a90ad3?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ffTR6iVy4mgY~LgUVGP6ovb~2XlQ6Zg4o1TnXA0KtDIf8vFxD9PzO1COkyjqoTamHEGk8HT7SH6X7xC7zaqrhCF7SNwOn6Iqnb4NqRQhaAGnlfOFUGXv54O9LnQcm5NxS-eJwrqmRwiaDiPK4-aqI9sgs6Il0-W1I8YNgXSHeyH5KDVukrkon3lZwH4yMjVeatkoiSMwMWbDz9EEYbgVFT2bfqtf9ubM8BKUeYG6UBMOit0hNtL0BIgaEPOFVr8fgwVc6ahO3-nUkLHibB-M2ecx5SN9N-ev5DSSbkIb3Dn43-c1-v~LyN491zw9c7GDPgf1F0Tcexhd5460l-VR2A__" alt="" />
+            </div>
+        </div>
+        <div class="flex justify-center mt-40 gap-24">
+            <div>
+            <img class="h-[600px]" src="https://s3-alpha-sig.figma.com/img/d8db/3a59/27a610b5397829ded3a4b7716b5cacce?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lxB1xc2JCsUP6Vpv4RUVY2GZ~IDiNDVN40A2jVX9WxiINT3JvbjVd9r8iz8xHwNpuLQW3piqAw~kJW6-LqbVoYMT9fUuI-lShdfe7HR6QuZB4SXDQ~~dbaTFny51ShwuwuKSQCNceYkrDCriz7jDb74b-uX16xygpUUyKirdDrM3vhpc3uLhYDUUZ-WFnXkYmyXzugUaGahQ3U6xXaP0wXsRCpwiMAH4HrtMXvtbbcx3Nif2Uu3SCWZk8KdcS560TdmVhcRvjUKja-qKx6n2Veyzh~EfFmrVZ5WoyGCfxqi2yM0tUdEEO7ThfYMXcRvWCpXq4UMzxiNBUnprZ4K9Vg__" alt="" />
+            </div>
+            <div>
+                <div class="text-9xl font-medium">Zero Design</div>
+                <div class="mt-10  text-2xl">
+                Includes a total of 10 static pages, divided like this: 4 Landing  <br />  pages, 2 About pages, 2 Contact page, 1 Team page, 1 FAQ  <br /> page. In addition to these, there are Webflow CMS driven pages  <br />: Blog (in 2 variations), Shop (in 2 variations), Works and Careers. <br /> All with their respective single pages!
+                </div>
+                <div>
+                    <div class="flex mt-7 gap-7">
+                        <div class=" h-12 text-white flex justify-center text-center w-32 bg-black pt-3">4 LANDING</div>
+                        <div class=" h-12 text-white flex justify-center text-center w-32 bg-black pt-3">2 ABOUT</div>
+                        <div class=" h-12 text-white flex justify-center text-center w-32 bg-black pt-3">3 CONTACT</div>
+                       
+                    </div>
+                    <div class="flex mt-3  gap-7">
+                    <div class=" h-12 text-white flex justify-center text-center w-32 bg-black pt-3">3 BLOG</div>
+                    <div class="h-12 text-white flex justify-center text-center w-32 bg-black pt-3">2 SHOP</div>
+                    </div>
+                    <div class="flex mt-3  gap-7">
+                    <div class=" h-12 text-white flex justify-center text-center w-32 bg-black pt-3">WORKS</div>
+                    <div class=" h-12 text-white flex justify-center text-center w-32 bg-black pt-3">TEAM</div>
+                    <div class=" h-12 text-white flex justify-center text-center w-32 bg-black pt-3">CAREERS</div>
+                    </div>
+                    <div class="flex mt-3   gap-7">
+                    <div class=" h-12 text-white flex justify-center text-center w-32 bg-black pt-3">FORMS</div>
+                    <div class=" h-12 text-white flex justify-center text-center w-32 bg-black pt-3">UTILITY</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ml-20 text-2xl mt-16 h-20 text-white flex justify-center text-center w-52 bg-black pt-6">LANDING PAGES</div>
+        <div class="flex justify-center gap-24 mt-24">
+            <div>
+            <img class="h-[500px]" src="https://s3-alpha-sig.figma.com/img/5750/6861/c61f8cd14133e5a8592e477a6a4b3f65?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=E8v6HDEiDGfkK5Wufu1RtYVBudb19UgESwskApe9i8k5USyz75OlO1F6vZ4CtNX~704InHVYvgW224H1iygMhl~6axkmaMo8dx1wYRzW1jDEbiGj1~IYQ10fM7~LmLiDS7o8mMsUc3AIlOn5L0sKsMveEj5YNkGW4HauVf~qEkSa0YV6SL0gzkJSUb9OgL7uMNqzJHr85nvxUx~0N0QojSU8~Ov5z0SesDRV~Os314Y3s17nX9F8O3QmYrNIH0g7mLCeZcOSm3gRqxDYBYfGYlRk7ewA6qvIVNI1E5IbpYqkedt7m1O~7fS-NHCDHqqQe-gi7lXXenkh09eJxY~6dg__" alt="" />
+            <h1 class="text-3xl font-normal">Landing 1</h1>
+            <br />
+            <img class="h-[500px]" src="https://s3-alpha-sig.figma.com/img/b19f/046f/599ba940eed4379beb41c04576473edf?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mP49HkfJ3sukInwtEP1MCDm4VWW2GrYHshrl6GtmTsdsogDnIo7~yKHoP5y33YTcSpfuNU1mG61DMiC96pR7gvgFD06pSfeh8zBouUn0rnRzh0YK7orsj0p84BHbXGnsEmqmqGuoLNRQyM4kYy2n62skSjpKX37PnpJpPP~zy0K34mEL0fd8bmEdgON2Lp9t4wU5~zk70GON9V-hLxApFqiDuUlqL5j~55lvuSr3sTtMdmzkqy0R86EgA7wi5PPR1qMF84eZS-z1j6m0TsImM-o2DnoAbSsuLEK0vJxOmfZsqcGXTRkS2RIqaXarDW1YnrDit-x6R6Z4uFacFtxOCA__" alt="" />
+            <h1  class="text-3xl font-normal">Landing 3</h1>
+            </div>
+            <div>
+            <img class="h-[500px]" src="https://s3-alpha-sig.figma.com/img/3901/e9dc/d672478ef01bba9f115aedbd2c21363d?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GhkP6V89nLgqpdwrbcVsqHhzwUIwnNHdCcN9E5n~AEaTqPeOT1Ma7Rzwmscm9auLt8IvJGIm98s7GsFRpMMIuhPLcQMcNifrJMD8~zaDIJdKEqMovtbqUw4ARmsEi1v1bzi-Uy0lBvCGa6MD6Hn~ytH6DQeT~lGqdwAcJkD2kV6NM4TOTBQK4jxtW0hffKuvTuKJoUi5DJjHLSdjV-1Eg5V0oYV3vbE1~rHOLkwQYtf67giXiPR~VSdUBi33A3ogvAYgQz5RSozi0kO7UEgy~17HNYirYUGdszkuJUkg32twSaWimIoLVjZN9ep7VL-Ag8rSRo6py9qlFF91qOXgqQ__" alt="" />
+            <h1  class="text-3xl font-normal">Landing 2</h1>
+            <br />
+            <img class="h-[500px]" src="https://s3-alpha-sig.figma.com/img/43ab/df54/d31322a39b9c4aa177a9aa229ac61d95?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DQ8-CJ8EoQTw~gYceVIgsMXqca2VFZVzthBOgtQtAECbw5b~weGd~XeSqRPWTLB0gNXIe5I6emledqfihS~gqW3kq73H8C-Qm-Ipy2ISHOYrDhfSuQOi8pCosplQ0W242Sfj6P0N0OC9-9e~8Fkh1GqIyVz~l8NMeS7M3Oksl66wM~ljJ7aebRzzIilBnb-tY72~zy61yu0iS2nxs18VaeYFouzhi9y01Ha2qJfDt4SKTduoEj9ToRwzOaPfBRnOgNPL5iVUdlM3d8UDAZP-NCVZaPflOD4jyFPQKvtoezOszlbgNuelaUx5TstQEfpJ2em0DiUDCsw5EFZgCPWOMQ__" alt="" />
+            <h1  class="text-3xl font-normal">Landing 4</h1>
+            </div>
+        </div>
+        </div>
+    )
 }
+export default vdc;
